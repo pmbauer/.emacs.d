86974584074fd6a19dc18116300b4e0eccdd28bb
pmbauer <pmbauer@capsaicin.(none)>
2012-04-02 08:08:45 -0700

    initial

---
+++ init.el
@@ -0,0 +1,46 @@
+;; packages
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings undo-tree clojure-mode midje-mode))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; load-path extensions
+(add-to-list 'load-path "~/.emacs.d/evil/lib")
+(add-to-list 'load-path "~/.emacs.d/evil")
+(add-to-list 'load-path "~/.emacs.d/extras")
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(add-to-list 'load-path "/opt/lono-dev-env/emacs")
+
+;; lonopair
+(require 'lonopair)
+
+;; evil mode
+(require 'evil)
+(evil-mode 1)
+
+;; visual
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-xemacs)))
+(set-default-font "Consolas-11")
+(blink-cursor-mode)
+
+;; prevent extraneous tabs
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+;; setup dired+
+(require 'dired+)
+
+;; clojure related
+(require 'slamhound)
