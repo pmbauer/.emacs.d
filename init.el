9fbe53e419507fc4f2e2de3e4a7c00a023c7251a
Paul M Bauer <pbauer@lonocloud.com>
2012-07-12 23:20:55 -0700

    removed duplicate customizations, added binding for slime-send-dwm

--- init.el
+++ init.el
@@ -26,6 +26,9 @@
 (require 'evil)
 (evil-mode 1)
 (define-key evil-normal-state-map (kbd "M-]") 'find-tag)
+(define-key evil-normal-state-map (kbd "M-,") 'slime-pop-find-definition-stack)
+(define-key evil-normal-state-map (kbd "M-.") 'slime-edit-definition)
+(define-key evil-normal-state-map (kbd "C-x M-x") 'slime-send-dwim)
 
 ;; visual
 (require 'color-theme)
@@ -33,69 +36,11 @@
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-solarized-light)
+     ;(color-theme-solarized-light)
      ))
 
 (set-default-font "Monospace-10")
 (blink-cursor-mode)
-
-(if window-system
-    (progn ; Non-terminal adjustments
-        )
-
-    (progn ; Terminal adjustments
-        ;; The light colored hl-line is fine for GUI emacs but makes
-        ;; it impossible to read things in a white-on-black terminal
-        (defadvice hl-line-mode (after
-                                 abrooks-advise-hl-line-mode
-                                 activate compile)
-          (set-face-background hl-line-face "white"))
-
-        ;; This is the coloring of the selection region
-        (face-spec-set 'region
-                       '((((class color) (background light))
-                          (;:foreground "blue"
-                           :background "color-17"
-                           :strike-through nil
-                           :underline nil))
-                         (t (:foreground "purple"
-                             :background "unspecified"
-                             :strike-through nil
-                             :underline t))))
-        ;; The idle-highlight is used by starter-kit... I think.
-        (face-spec-set 'idle-highlight
-                       '((((class color) (background light))
-                          (;:foreground "black"
-                           :background "gray25"
-                           :strike-through nil
-                           :underline nil))
-                         (t (:foreground "purple"
-                             :background "unspecified"
-                             :strike-through nil
-                             :underline t))))
-        ;; The idle-highlight is used by starter-kit... I think.
-        (face-spec-set 'show-paren-match
-                       '((((class color) (background light))
-                          (:foreground "yellow"
-                           :background nil
-                           :bold t
-                           :strike-through nil
-                           :underline nil))
-                         (t (:foreground "purple"
-                             :background "unspecified"
-                             :strike-through nil
-                             :underline t))))
-        ;; This makes our trailing whitespace actually visible
-        (face-spec-set 'trailing-whitespace
-                       '((((class color) (background light))
-                          (;:foreground "red"
-                           :background "red"
-                           :strike-through nil
-                           :underline nil))
-                         (t (;:foreground "red"
-                             :background "red"
-                             :strike-through nil
-                             :underline nil))))))
 
 ;; prevent extraneous tabs
 (setq-default tab-width 4)
@@ -106,11 +51,3 @@
 
 ;; clojure related
 (require 'slamhound)
-
-(setq path-to-ctags "/usr/bin/ctags")
-(defun create-clj-tags (dir-name)
-  "Create tags file."
-  (interactive "Directory: ")
-  (shell-command
-   (format "%s --langmap=Lisp:+.clj --regex-Lisp='/[ \t\(]*def[a-z]* \([a-z!-]+\)/\1/'  --regex-Lisp='/[ \t\(]*ns \([a-z.]+\)/\1/' -f %s/TAGS -e -R %s" path-to-ctags dir-name (directory-file-name dir-name))))
-
