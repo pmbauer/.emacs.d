e69ef95237b53203888b5d234d2c0e42791e1a8f
Paul M Bauer <paul.michael.bauer@gmail.com>
2012-11-14 22:05:49 -0700

    elpa: updated, added nrepl

--- elpa/smex-2.0/smex-autoloads.el
+++ elpa/smex-2.0/smex-autoloads.el
@@ -3,8 +3,14 @@
 ;;; Code:
 
 
-;;;### (autoloads (smex-initialize) "smex" "smex.el" (20205 30533))
+;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (20644
+;;;;;;  28834))
 ;;; Generated autoloads from smex.el
+
+(autoload 'smex "smex" "\
+
+
+\(fn)" t nil)
 
 (autoload 'smex-initialize "smex" "\
 
@@ -13,7 +19,7 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("smex-pkg.el") (20205 30533 509393))
+;;;### (autoloads nil nil ("smex-pkg.el") (20644 28834 560028))
 
 ;;;***
 
