dfef17eeaf1a5eaf1f8087f3f9f2228000bf9369
Paul M Bauer <paul.michael.bauer@gmail.com>
2013-02-26 10:58:35 -0700

    upgrade packages, enable auto complete and nrepl

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings slime slime-repl undo-tree clojure-mode clojure-test-mode color-theme-solarized haskell-mode))
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings slime slime-repl undo-tree clojure-mode clojure-test-mode color-theme-solarized haskell-mode auto-complete ac-nrepl))
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
@@ -29,20 +29,77 @@
 
 ;; lonopair
 (require 'lonopair)
-(require 'lono-slime-extensions)
+(defun lono-slime-hook-function ()
+  (local-set-key (kbd "C-c C-s") 'nrepl-send-dwim))
+
+;; paredit tweaks
+(defun paredit-duplicate-after-point
+  ()
+  "Duplicates the content of the line that is after the point."
+  (interactive)
+  ;; skips to the next sexp
+  (while (looking-at " ")
+    (forward-char))
+  (set-mark-command nil)
+  ;; while we find sexps we move forward on the line
+  (while (and (bounds-of-thing-at-point 'sexp)
+              (<= (point) (car (bounds-of-thing-at-point 'sexp)))
+              (not (= (point) (line-end-position))))
+    (forward-sexp)
+    (while (looking-at " ")
+      (forward-char)))
+  (kill-ring-save (mark) (point))
+  ;; go to the next line and copy the sexprs we encountered
+  (paredit-newline)
+  (yank)
+  (exchange-point-and-mark))
+
+(defun paredit-wrap-round-from-behind ()
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (insert " ")
+  (forward-char -1))
+
+(eval-after-load "paredit"
+  '(progn
+     ;;(define-key paredit-mode-map (kbd "C-c 0") 'paredit-forward-slurp-sexp)
+     ;;(define-key paredit-mode-map (kbd "C-c )") 'paredit-forward-barf-sexp)
+     ;;(define-key paredit-mode-map (kbd "C-c 9") 'paredit-backward-slurp-sexp)
+     ;;(define-key paredit-mode-map (kbd "C-c (") 'paredit-backward-barf-sexp)
+     ;;(define-key paredit-mode-map (kbd "M-R") 'paredit-raise-sexp)
+     ;;(define-key paredit-mode-map (kbd "C-k") 'paredit-eager-kill-line)
+     (define-key paredit-mode-map (kbd "C-S-d") 'paredit-duplicate-after-point)
+     ;;(define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind)
+     ))
 
 ;; evil mode
 (require 'evil)
 (evil-mode 1)
 (define-key evil-normal-state-map (kbd "M-]") 'find-tag)
-;(define-key evil-normal-state-map (kbd "M-,") 'nrepl-jump-back)
-;(define-key evil-normal-state-map (kbd "M-.") 'nrepl-jump)
-(define-key evil-normal-state-map (kbd "M-,") 'slime-pop-find-definition-stack)
-(define-key evil-normal-state-map (kbd "M-.") 'slime-edit-definition)
-(define-key evil-normal-state-map (kbd "C-x M-x") 'slime-send-dwim)
+(define-key evil-normal-state-map (kbd "M-,") 'nrepl-jump-back)
+(define-key evil-normal-state-map (kbd "M-.") 'nrepl-jump)
+(define-key evil-normal-state-map (kbd "C-x M-x") 'nrepl-send-dwim)
+;(define-key evil-normal-state-map (kbd "M-,") 'slime-pop-find-definition-stack)
+;(define-key evil-normal-state-map (kbd "M-.") 'slime-edit-definition)
+;(define-key evil-normal-state-map (kbd "C-x M-x") 'slime-send-dwim)
 
 ;; nrepl
-;(setq nrepl-popup-stacktraces nil)
+(setq nrepl-popup-stacktraces nil)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+; autocomplete
+(require 'ac-nrepl)
+ (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+ (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+ (eval-after-load "auto-complete"
+   '(add-to-list 'ac-modes 'nrepl-mode))
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 ;; visual
 (require 'color-theme)
