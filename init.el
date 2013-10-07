8ba3a637664ccc1dcf2dcbf93640c22bf94ccd41
Paul M Bauer <paul.michael.bauer@gmail.com>
2013-10-07 09:34:25 -0700

    smart parens

--- init.el
+++ init.el
@@ -18,7 +18,8 @@
              haskell-mode
              markdown-mode
              auto-complete
-             ac-nrepl))
+             ac-nrepl
+             smartparens))
   (when (not (package-installed-p p))
     (package-install p)))
 
@@ -45,6 +46,65 @@
 
 (add-hook 'nrepl-interaction-mode-hook 'lono-nrepl-hook-function)
 
+;; smart parens, html editing
+(require 'smartparens)
+
+;; make sp-select-next-thing works even the cusor is in the open/close tag
+;; like matchit in vim
+;; @return t => start from open tag; nil start from close tag
+(defun my-sp-select-next-thing (&optional NUM)
+  (interactive "p")
+  (let ((b (line-beginning-position))
+        (e (line-end-position))
+        (char (following-char))
+        (p (point))
+        rbeg
+        rend
+        (rlt t))
+    ;; "<" char code is 60
+    ;; search backward
+    (if (not (= char 60))
+        (save-excursion
+          (while (and (<= b (point)) (not (= char 60)))
+            (setq char (following-char))
+            (setq p (point))
+            (backward-char))))
+    ;; search forward
+    (if (not (= char 60))
+        (save-excursion
+          (while (and (>= e (point)) (not (= char 60)))
+            (setq char (following-char))
+            (setq p (point))
+            (forward-char))))
+    ;; do the real thing
+    (when (and (= char 60) (< p e))
+      (goto-char p)
+      (forward-char)
+      (if (= (following-char) 47)
+          (progn
+            ;; </
+            (backward-char)
+            (setq rlt nil))
+        (progn
+          ;; < , looks fine
+          (backward-char)
+          (setq rlt t)))
+      (sp-select-next-thing)
+      (setq rbeg (region-beginning))
+      (setq rend (region-end))
+
+      (while (> NUM 1)
+        ;; well, sp-select-next-thing is kind of wierd
+        (re-search-forward "<[^!]")
+        (backward-char 2)
+        (sp-select-next-thing)
+        (setq rend (region-end))
+        (setq NUM (1- NUM)))
+      (push-mark rbeg t t)
+      (goto-char (1-rend)))
+    rlt))
+
+;; paredit customizations
 (defun paredit-wrap-round-from-behind ()
   (interactive)
   (forward-sexp -1)
@@ -66,6 +126,7 @@
 ;; evil mode
 (require 'evil)
 (evil-mode 1)
+
 (define-key evil-normal-state-map (kbd "M-]") 'find-tag)
 (define-key evil-normal-state-map (kbd "M-,") 'nrepl-jump-back)
 (define-key evil-normal-state-map (kbd "M-.") 'nrepl-jump)
@@ -73,6 +134,21 @@
 ;(define-key evil-normal-state-map (kbd "M-,") 'slime-pop-find-definition-stack)
 ;(define-key evil-normal-state-map (kbd "M-.") 'slime-edit-definition)
 ;(define-key evil-normal-state-map (kbd "C-x M-x") 'slime-send-dwim)
+
+;; {{ evil-matchit
+(defun my-evil-jump-item-enhanced-for-html ()
+  (interactive)
+  (if (or (eq major-mode 'html-mode)
+          (eq major-mode 'xml-mode)
+          (eq major-mode 'nxml-mode))
+      (progn
+        (if (not (my-sp-select-next-thing 1)) (exchange-point-and-mark))
+        (deactivate-mark))
+    (progn
+      (evil-jump-item))))
+
+(define-key evil-normal-state-map "%" 'my-evil-jump-item-enhanced-for-html)
+;; }}
 
 ;; nrepl
 
