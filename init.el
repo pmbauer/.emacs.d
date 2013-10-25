ee0d3a215f126c06a29fa04522981eba65f70438
Paul M Bauer <paul.michael.bauer@gmail.com>
2013-10-25 08:54:27 -0700

    update to nrepl 0.2.0

--- init.el
+++ init.el
@@ -151,6 +151,7 @@
 ;; }}
 
 ;; nrepl
+(require 'nrepl)
 
 (defun nrepl-exec-interactive (cmd)
   (interactive)
@@ -171,20 +172,24 @@
   (nrepl-exec-interactive "(user/reset)"))
 
 (setq nrepl-popup-stacktraces nil)
+(setq nrepl-history-file "~/.nrepl-history.eld")
 
-(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'nrepl-repl-mode-hook 'paredit-mode)
+
+(global-set-key (kbd "<f5>") 'nrepl-refresh)
+(global-set-key (kbd "<f8>") 'nrepl-reset)
 
 ; autocomplete
 (require 'ac-nrepl)
- (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+ (add-hook 'nrepl-repl-mode-hook 'ac-nrepl-setup)
  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
  (eval-after-load "auto-complete"
-   '(add-to-list 'ac-modes 'nrepl-mode))
+   '(add-to-list 'ac-modes 'nrepl-repl-mode))
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
